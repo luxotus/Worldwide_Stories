178ac384a509f934d3572e32ed4f6adbe502185d
Felix Manuel <luxotus@gmail.com>
2018-06-03 15:42:36 -0400

    Added feature sections and included icon fonts. Also used illustrator to put screenshots on phone outlines. I'll still have to do more tweaks to images.

--- resources/views/home.php
+++ resources/views/home.php
@@ -38,6 +38,54 @@
 					<img src="<?php print asset('images/icons/play_icon.png'); ?>">
 				</a>
 			</div>
+			<div class="row feature-section feature-countries">
+				<div class="col-sm-6 image-section left-section text-center">
+					<img src="<?php print asset('images/screens/countries_phone.png'); ?>">
+				</div>
+				<div class="col-sm-6 text-section right-section">
+					<div class="text-center title-icon-section">
+						<span class="icon-globe"></span>
+						<h2>Countries</h2>
+					</div>
+					<p>Create lists to organize the things you love. Restaurants, Films, Music, Shopping, Friends. We've integrated with partners to make your lists rich and actionable automatically. For example: Add a restaurant and watch it populate with location information, reviews, photos and then actions, like "call for a reservation."</p>
+				</div>
+			</div>
+			<div class="row feature-section feature-stories">
+				<div class="col-sm-6 text-section left-section">
+					<div class="text-center title-icon-section">
+						<span class="icon-book"></span>
+						<h2>Stories</h2>
+					</div>
+					<p>Create lists to organize the things you love. Restaurants, Films, Music, Shopping, Friends. We've integrated with partners to make your lists rich and actionable automatically. For example: Add a restaurant and watch it populate with location information, reviews, photos and then actions, like "call for a reservation."</p>
+				</div>
+				<div class="col-sm-6 image-section right-section">
+					<img src="<?php print asset('images/screens/story_phone.png'); ?>">
+				</div>
+			</div>
+			<div class="row feature-section feature-bookmarks">
+				<div class="col-sm-6 image-section left-section text-center">
+					<img src="<?php print asset('images/screens/bookmarks_phone.png'); ?>">
+				</div>
+				<div class="col-sm-6 text-section right-section">
+					<div class="text-center title-icon-section">
+						<span class="icon-bookmark"></span>
+						<h2>Bookmarking</h2>
+					</div>
+					<p>Create lists to organize the things you love. Restaurants, Films, Music, Shopping, Friends. We've integrated with partners to make your lists rich and actionable automatically. For example: Add a restaurant and watch it populate with location information, reviews, photos and then actions, like "call for a reservation."</p>
+				</div>
+			</div>
+			<div class="row feature-section feature-share">
+				<div class="col-sm-6 text-section left-section">
+					<div class="text-center title-icon-section">
+						<span class="icon-share"></span>
+						<h2>Share</h2>
+					</div>
+					<p>Create lists to organize the things you love. Restaurants, Films, Music, Shopping, Friends. We've integrated with partners to make your lists rich and actionable automatically. For example: Add a restaurant and watch it populate with location information, reviews, photos and then actions, like "call for a reservation."</p>
+				</div>
+				<div class="col-sm-6 image-section right-section">
+					<img src="<?php print asset('images/screens/share_phone.png'); ?>">
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
